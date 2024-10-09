f9bcf7daeb7967ea1107937309468b4a15803bf4
adla.mtawea <adla.mtawea@gotocme.com>
2024-10-09 12:30:05 +0300

    add ability to and save question history + download

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import data_handler
 import openai_handler
 import logging
+import uuid
+import os
 
 # Setup logging
 logging.basicConfig(
@@ -25,6 +27,33 @@ def load_csv():
 # Title of the app
 st.title("POC - OpenAI Table Analysis")
 logger.info("App started")
+def check_first_load():
+    if 'first_load' not in st.session_state:
+        st.session_state.first_load = True
+        st.session_state.thread_id = uuid.uuid1().hex
+        st.session_state.file_content = ""
+    else:
+        st.session_state.first_load = False
+
+# Call the function
+check_first_load()
+logger.error(st.session_state.first_load)
+
+try:
+    # Display status
+    #if st.session_state.first_load:
+    #    thread_id = uuid.uuid1().hex
+    if os.path.exists(st.session_state.thread_id+'_data.json'):
+        with open(st.session_state.thread_id+'_data.json', 'r') as file:
+            st.session_state.file_content = file.read()
+    if st.session_state.file_content!="":             
+        button_clicked = st.download_button(label="Download File", data=st.session_state.file_content, file_name="data.txt")#, # Name of the file to download mime="text/plain" # MIME type for plain text ):
+        if button_clicked:
+            st.session_state.first_load = True
+            st.session_state.thread_id = uuid.uuid1().hex
+            st.session_state.file_content = ""
+except Exception as e:
+    logger.error("Exception occurred: %s", str(e))
 
 try:
     # Load the CSV using caching
@@ -33,7 +62,7 @@ try:
     
     # Input for custom question
     question = st.text_input("Enter your question for OpenAI:", "Give me all the tables that are commercial.")
-    
+
     if st.button("Run Analysis"):
         logger.info("Analysis button clicked with question: %s", question)
 
@@ -50,17 +79,17 @@ try:
         
         try:
             logger.info("Sending data to OpenAI with question: %s", question)
-            response_data = openai_handler.open_ai_run_analysis(table_data, question)
+            response_data = openai_handler.open_ai_run_analysis(table_data, question, st.session_state.thread_id)
             logger.info("OpenAI response received")
 
             # Extract the response and display it
-            response = response_data.choices[0].message.content
+            response = response_data#.choices[0].message.content
             st.write("OpenAI Response:")
             st.write(response)
             logger.info("OpenAI response displayed successfully")
-        except openai.error.OpenAIError as api_error:
-            st.error("An OpenAI error occurred: " + str(api_error))
-            logger.error("OpenAIError: %s", str(api_error))
+        # except openai.error.OpenAIError as api_error:
+        #     st.error("An OpenAI error occurred: " + str(api_error))
+        #     logger.error("OpenAIError: %s", str(api_error))
         except Exception as e:
             st.error(f"An error occurred while communicating with OpenAI: {str(e)}")
             logger.error("Exception during OpenAI call: %s", str(e))
--- openai_handler.py
+++ openai_handler.py
@@ -1,10 +1,22 @@
-from openai import OpenAI
+#from openai import OpenAI
 import pandas as pd
 import cryptography_handler
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage, SystemMessage
+import getpass
+import os
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import START, MessagesState, StateGraph
 
 encrypted_val = b'gAAAAABm9fuEW69QQqx9iJj-d7mprLSD_YGeHMmK4MN91z_CwEAJ6YK9dC2v8_9OXfOVx2My8NV-whI0Nff-OG1iJCJcxh8Mvltm4ATeto4Gl8Rvph9JHbRsMEUHsfaFTmmvLXy0WBhwTIwfslvaytSkzk6kOEWNxjh8t4XZZKY3bebAptDnyNn2leK9tZIdRzTV1iiHym987eA-HmvmPx6RUdWrJHuP3W0g8OAgVG5cUJbth2WeAIM0K5IqBFqarvlvDw7RYVDPnPRy2pwq6VaGrzfTXZDQ4mf6wvQ8NHddIfvQbmPHqpE='
 key = cryptography_handler.load_key()
 open_ai_key = cryptography_handler.decrypt_api_key(encrypted_val, key)
+
+os.environ["OPENAI_API_KEY"] = open_ai_key
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_API_KEY"] = getpass.getpass()
+
+model = ChatOpenAI(model="gpt-4o-mini")
 
 # Function to create the prompt for OpenAI
 def create_prompt(table_data, input_text):
@@ -15,16 +27,51 @@ def create_prompt(table_data, input_text):
         prompt += f"Sample Data (2 records): {table['sample_data']}\n"
     return prompt
 
+# Define the function that calls the model
+def call_model(state: MessagesState):
+    response = model.invoke(state["messages"])
+    return {"messages": response}
 
-def open_ai_run_analysis(table_data, input_text):
+# Define a new graph
+workflow = StateGraph(state_schema=MessagesState)
+# Define the (single) node in the graph
+workflow.add_edge(START, "model")
+workflow.add_node("model", call_model)
+# Add memory
+memory = MemorySaver()
+app = workflow.compile(checkpointer=memory)
+
+def open_ai_run_analysis(table_data, input_text, thread_id):
+    config = {"configurable": {"thread_id": thread_id}}
     prompt = create_prompt(table_data, input_text)
-    client = OpenAI(api_key= open_ai_key)
-    # Make the API call to OpenAI
-    response =  client.chat.completions.with_raw_response.create(
-    messages=[
-            {"role": "system", "content": "You are a database architect."},
-            {"role": "user", "content": prompt}
-        ],
-    model="gpt-4o-mini").parse()
-    return response
-    
+    messages = [
+        SystemMessage(content="You are a database architect"),
+        HumanMessage(content=prompt),
+    ]
+    output = app.invoke({"messages": messages}, config)
+    save_content(input_text, output["messages"][-1].content,thread_id)
+    return output["messages"][-1].content
+
+def save_content(question, answer, thread_id):
+    with open(thread_id+"_data.json", "a") as file:
+            file.write(question+"\n")
+            file.write(answer+"\n")
+
+
+# def save_content(thread_id, Q_A):
+#     with open(thread_id+"_data.json", "w") as file:
+#         for index, line in enumerate(Q_A["messages"]):
+#             if index > 0:
+#                 statment = line.content
+#                 if isinstance(line, HumanMessage):
+#                     statment = substring_until_keyword(line.content, "\n\nTable Name")
+#                 file.write(statment)
+#                 file.write("================================")
+
+# def substring_until_keyword(sentence, keyword):
+#     index = sentence.find(keyword)
+#     if index != -1:
+#         return sentence[:index]
+#     else:
+#         return sentence  # Return the whole string if the keyword is not found
+
